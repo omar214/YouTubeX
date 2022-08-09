737a3937bc144cd7d19765e8360dd61c8474e821
omar214 <omarmohamed2322000@gmail.com>
2022-08-09 22:29:36 +0200

    Side menu Component✅

---
+++ Client/src/components/Menu.jsx
@@ -0,0 +1,153 @@
+// impot Icons
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
+import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
+import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import SportsBasketballOutlinedIcon from "@mui/icons-material/SportsBasketballOutlined";
+import MovieOutlinedIcon from "@mui/icons-material/MovieOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
+
+import youtubeLogo from "../img/logo.png";
+import { Link, useLocation } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  Container,
+  Wrapper,
+  Logo,
+  Img,
+  Item,
+  Hr,
+  Login,
+  Button,
+  Title,
+} from "../styles/Menu.styled.js";
+
+const Menu = ({ darkMode, setDarkMode }) => {
+  const location = useLocation();
+  const path = location.pathname;
+  const { currentUser } = useSelector((state) => state.user);
+  // const dispatch = useDispatch();
+
+  return (
+    <Container id="mySidenav">
+      <Wrapper>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <Logo>
+            <Img src={youtubeLogo} />
+            youtube-Clone
+          </Logo>
+        </Link>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <Item current={path === "/"}>
+            <HomeIcon />
+            Home
+          </Item>
+        </Link>
+        <Link to="/trend" style={{ textDecoration: "none", color: "inherit" }}>
+          <Item current={path === "/trend"}>
+            <ExploreOutlinedIcon />
+            Explore
+          </Item>
+        </Link>
+        <Link
+          to="/subscribes"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <Item current={path === "/subscribes"}>
+            <SubscriptionsOutlinedIcon />
+            Subscriptions
+          </Item>
+        </Link>
+        <Hr />
+        <Link
+          to="/library"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <Item>
+            <VideoLibraryOutlinedIcon />
+            Saved Videos
+          </Item>
+        </Link>
+        <Link
+          to="/history"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <Item>
+            <HistoryOutlinedIcon />
+            History
+          </Item>
+        </Link>
+        <Hr />
+        {currentUser ? (
+          currentUser.email
+        ) : (
+          <>
+            <Login>
+              Sign in to like videos, comment, and subscribe.
+              <Link to="signin" style={{ textDecoration: "none" }}>
+                <Button>
+                  <AccountCircleOutlinedIcon />
+                  SIGN IN
+                </Button>
+              </Link>
+            </Login>
+          </>
+        )}
+        <Hr />
+        <Title>BEST OF youtube-Clone</Title>
+        <Item>
+          <LibraryMusicOutlinedIcon />
+          Music
+        </Item>
+        <Item>
+          <SportsBasketballOutlinedIcon />
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsOutlinedIcon />
+          Gaming
+        </Item>
+        <Item>
+          <MovieOutlinedIcon />
+          Movies
+        </Item>
+        <Item>
+          <ArticleOutlinedIcon />
+          News
+        </Item>
+        <Item>
+          <LiveTvOutlinedIcon />
+          Live
+        </Item>
+        <Hr />
+        <Item>
+          <SettingsOutlinedIcon />
+          Settings
+        </Item>
+        <Item>
+          <FlagOutlinedIcon />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineOutlinedIcon />
+          Help
+        </Item>
+        <Item onClick={() => setDarkMode(!darkMode)}>
+          <SettingsBrightnessOutlinedIcon />
+          {darkMode ? "Light" : "Dark"} Mode
+        </Item>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Menu;
