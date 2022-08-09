19d1599b3984461045a320bca48aa364289c8e7e
omar214 <omarmohamed2322000@gmail.com>
2022-08-09 22:33:30 +0200

    upload Component && uploading Logic

---
+++ Client/src/components/Upload.jsx
@@ -0,0 +1,165 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
+import {
+  Container,
+  Wrapper,
+  Close,
+  Title,
+  Input,
+  Desc,
+  Button,
+  Label,
+} from "../styles/upload.styled.js";
+import firebaseApp from "../firebase/firebase.js";
+import API from "../api/api.js";
+import { useSelector } from "react-redux";
+
+const Upload = ({ setOpen }) => {
+  const { currentUser } = useSelector((state) => state.user);
+
+  const [img, setImg] = useState(undefined);
+  const [video, setVideo] = useState(undefined);
+  const [imgPerc, setImgPerc] = useState(0);
+  const [videoPerc, setVideoPerc] = useState(0);
+  const [inputs, setInputs] = useState({});
+  const [tags, setTags] = useState([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleKeyDown = (e) => e.keyCode === 27 && setOpen(false);
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [setOpen]);
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleTags = (e) => {
+    setTags(e.target.value.split(","));
+    console.log(currentUser);
+    console.log(tags);
+  };
+
+  const uploadFile = (file, urlType) => {
+    const storage = getStorage(firebaseApp);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        urlType === "imgUrl"
+          ? setImgPerc(Math.round(progress))
+          : setVideoPerc(Math.round(progress));
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+            break;
+        }
+      },
+      (error) => {},
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setInputs((prev) => {
+            return { ...prev, [urlType]: downloadURL };
+          });
+        });
+      }
+    );
+  };
+
+  useEffect(() => {
+    video && uploadFile(video, "videoUrl");
+  }, [video]);
+
+  useEffect(() => {
+    img && uploadFile(img, "imgUrl");
+  }, [img]);
+
+  const handleUpload = async (e) => {
+    e.preventDefault();
+    console.log(inputs, tags);
+    // if (!img || !video) return;
+
+    const res = await API.post("/videos", {
+      userId: currentUser._id,
+      ...inputs,
+      tags,
+    });
+    console.log(res);
+    setOpen(false);
+    res.status === 200 && navigate(`/`);
+  };
+
+  return (
+    <Container>
+      <Wrapper>
+        <Close onClick={() => setOpen(false)}>X</Close>
+        <Title>Upload a New Video</Title>
+        <Label>Video:</Label>
+        {videoPerc > 0 ? (
+          "Uploading:" + videoPerc
+        ) : (
+          <Input
+            type="file"
+            accept="video/*"
+            onChange={(e) => setVideo(e.target.files[0])}
+          />
+        )}
+        <Input
+          type="text"
+          placeholder="Title"
+          name="title"
+          onChange={handleChange}
+        />
+        <Desc
+          placeholder="Description"
+          name="desc"
+          rows={8}
+          onChange={handleChange}
+        />
+        <Input
+          type="text"
+          placeholder="Separate the tags with commas."
+          onChange={handleTags}
+        />
+        <Label>Thumbnail :</Label>
+        {imgPerc > 0 ? (
+          "Uploading:" + imgPerc + "%"
+        ) : (
+          <Input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setImg(e.target.files[0])}
+          />
+        )}
+        <Button onClick={handleUpload}>Upload</Button>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Upload;
