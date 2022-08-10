abb63e843f8bfc03058c338f9c60de02e554dda2
omar214 <omarmohamed2322000@gmail.com>
2022-08-10 21:39:56 +0200

    protect route

--- Client/src/pages/history/History.jsx
+++ Client/src/pages/history/History.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import API from "../../api/api.js";
 import Card from "../../components/Card.jsx";
 
@@ -6,6 +8,12 @@ import { Container } from "./History.styled.js";
 
 const History = () => {
   const [videos, setVideos] = useState([]);
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!currentUser) navigate("/signin");
+  }, [currentUser, navigate]);
 
   useEffect(() => {
     const fetchVideos = async () => {
@@ -21,7 +29,9 @@ const History = () => {
 
   return (
     <Container>
-      {videos.length > 0 ? (
+      {!currentUser ? (
+        navigate("/signin")
+      ) : videos.length > 0 ? (
         videos.map((video) => <Card key={video.id} video={video} />)
       ) : (
         <h1>No videos in your history </h1>
